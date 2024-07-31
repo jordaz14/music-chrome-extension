a416e7558835d7b19b5757e8af8f07de4e63fb2c
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-07-30 21:57:51 -0400

    Add OAuth Implicit Flow & User Profile Endpoint

--- background.js
+++ background.js
@@ -28,7 +28,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 // Spotify API
 
 function authenticateSpotify() {
-  const clientID = "";
+  const clientID = "d3cfbf644eb24125af43c2032ac41d10";
   const redirectURI = `https://${chrome.runtime.id}.chromiumapp.org/`;
   const scopes = "user-read-private user-read-email";
   const authURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&redirect_uri=${encodeURIComponent(
@@ -52,8 +52,28 @@ function authenticateSpotify() {
         /access_token=([^&]*)/
       )[1];
       console.log("Access Token:", accessToken);
+      fetchUserProfile(accessToken);
 
-      chrome.storage.local.set({ spotifyAccessToken: accessToken });
+      //      chrome.storage.local.set({ spotifyAccessToken: accessToken });
     }
   );
+}
+
+async function fetchUserProfile(accessToken) {
+  try {
+    const response = await fetch("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch user profile");
+    }
+
+    const data = await response.json();
+    console.log("User Profile:", data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
