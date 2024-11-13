e8b3a5d8a018469575176eb97a8e21035d3b674c
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-11-12 22:16:06 -0500

    Refactor popup.js

--- popup.js
+++ popup.js
@@ -1,95 +1,26 @@
 const tabTitle = document.querySelector("#title");
-const moodDesc = document.querySelector("#mood");
 const songRec = document.querySelector("#song");
+const songRecDesc = document.querySelector("#mood");
 const spotifyButton = document.querySelector("#spotify-button");
-const openaiButton = document.querySelector("#openai-button");
-const form = document.querySelector("#filter-form");
+const filterForm = document.querySelector("#filter-form");
 
-/*
-const playButton = document.querySelector("#play-button");
-const pauseButton = document.querySelector("#pause-button");
-*/
-
+// Find active tab on load and update tab title
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  console.log("Chrome Tab Query Attempting...");
   const activeTab = tabs[0];
   tabTitle.textContent = `${activeTab.title}`;
 });
 
-/*
-playButton.addEventListener("click", () => {
-  console.log("Sending play command");
-  chrome.runtime.sendMessage({ action: "play" });
-});
-
-pauseButton.addEventListener("click", () => {
-  console.log("Sending pause command");
-  chrome.runtime.sendMessage({ action: "pause" });
-});
-*/
-
-//ChatGPT API
-
 main();
 
 async function main() {
-  console.log("Main Attempeting...");
-  const schemaJSON = createSchema();
-
-  const tabsResult = await new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (chrome.runtime.lastError) {
-        return reject(chrome.runtime.lastError);
-      }
-      resolve(tabs[0].title);
-    });
-  });
-
-  console.log(tabsResult);
-
-  const chatGPTResponse = await askChatGPT(
-    `Recommend a song which relates to ${tabTitle.textContent}. Explain your rationale in pne sentence.`,
-    schemaJSON
-  );
-
-  moodDesc.textContent = `${chatGPTResponse.trackChoice}`;
-  songRec.textContent = `Recommendation: ${chatGPTResponse.track} by ${chatGPTResponse.artist}`;
-
-  spotifyButton.addEventListener("click", () => {
-    chrome.runtime.sendMessage({
-      action: "authenticateSpotify",
-      payload: chatGPTResponse,
-    });
-  });
-}
-
-form.addEventListener("submit", handleFormSubmit);
-
-async function handleFormSubmit(e) {
-  e.preventDefault();
-
-  const formDataObj = {};
-  const formData = new FormData(form);
-  formData.forEach((value, key) => {
-    formDataObj[key] = value;
-  });
-
-  const schemaJSON = createSchema();
-
-  console.log(formDataObj);
-
-  if (formDataObj.filter == "genre" && formDataObj.input) {
-    schemaJSON.properties.track.description = `You must provide a song in this genre ${formDataObj.input}. No exceptions`;
-  } else if (formDataObj.filter == "artist" && formDataObj.input) {
-    schemaJSON.properties.track.description = `You must provide a song by ${formDataObj.input}. No exceptions`;
-  }
+  const schemaJSON = createChatSchema();
 
   const chatGPTResponse = await askChatGPT(
     `Recommend a song which relates to ${tabTitle.textContent}. Explain your rationale in one sentence.`,
     schemaJSON
   );
 
-  moodDesc.textContent = `${chatGPTResponse.trackChoice}`;
+  songRecDesc.textContent = `${chatGPTResponse.trackChoice}`;
   songRec.textContent = `Recommendation: ${chatGPTResponse.track} by ${chatGPTResponse.artist}`;
 
   spotifyButton.addEventListener("click", () => {
@@ -100,16 +31,41 @@ async function handleFormSubmit(e) {
   });
 }
 
-async function loadConfig() {
+filterForm.addEventListener("submit", handleFilterFormSubmit);
+async function handleFilterFormSubmit(e) {
+  e.preventDefault();
+
+  // Gather filter form data
+  const filterFormData = new FormData(filterForm);
+  const filterFormDataObj = Object.fromEntries(filterFormData.entries());
+
+  const chatSchema = createChatSchema();
+
+  // Update schema passed to ChatGPT based on user filter data
+  if (filterFormDataObj.filter == "genre" && filterFormDataObj.input) {
+    chatSchema.properties.track.description = `You must provide a song in this genre ${filterFormDataObj.input}. No exceptions`;
+  } else if (filterFormDataObj.filter == "artist" && filterFormDataObj.input) {
+    chatSchema.properties.track.description = `You must provide a song by ${filterFormDataObj.input}. No exceptions`;
+  }
+
+  // Prompt ChatGPT for song recommendation given user filters
   try {
-    const response = await fetch(chrome.runtime.getURL("config.json"));
-    if (!response.ok)
-      throw new Error(`Failed to load config: ${response.statusText}`);
-    const config = await response.json();
-    return config.API_KEY;
+    const chatGPTResponse = await askChatGPT(
+      `Recommend a song which relates to ${tabTitle.textContent}. Explain your rationale in one sentence.`,
+      chatSchema
+    );
+
+    songRecDesc.textContent = `${chatGPTResponse.trackChoice}`;
+    songRec.textContent = `Recommendation: ${chatGPTResponse.track} by ${chatGPTResponse.artist}`;
+
+    spotifyButton.addEventListener("click", () => {
+      chrome.runtime.sendMessage({
+        action: "authenticateSpotify",
+        payload: chatGPTResponse,
+      });
+    });
   } catch (error) {
-    console.error("Error loading configuration:", error);
-    return null;
+    console.error("Error occurred during ChatGPT Request:", error);
   }
 }
 
@@ -159,7 +115,6 @@ async function askChatGPT(message, schema) {
     const data = await response.json();
     const reply = data.choices[0].message.tool_calls[0].function.arguments;
     const jsonReply = JSON.parse(reply);
-    console.log(jsonReply);
     return jsonReply;
   } catch (error) {}
 }
@@ -205,6 +160,4 @@ function createChatSchema() {
     },
     required: ["track", "artist", "album", "genre", "trackChoice"],
   };
-
-  return schemaJSON;
 }
